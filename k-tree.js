addc8b657f31205ba1b49b1defd7ef5aa817261b
Ka.Z <imhazige@gmail.com>
2017-08-01 16:59:56 +0800

    clean step 2

--- k-tree.js
+++ k-tree.js
@@ -208,57 +208,6 @@
 		
 	};
 
-	kzg.Url = function (argOrginUrl) {
-		var _this = this;
-		var params = {};
-		var baseurl = null;
-
-		this.putParam = function (argKey, argValue) {
-			if (argKey && argValue) {
-				params[argKey] = argValue;
-			}
-		};
-		this.getParam = function (argKey) {
-			return params[argKey];
-		};
-
-		this.toString = function () {
-			var keys = params.keyset;
-			var url = baseurl;
-			var qs = "?";
-			for (k in params) {
-				var value = params[k];
-				if (value === 0 || value) {
-					url = url + qs + k + "=" + value;
-				}
-				qs = "&";
-			}
-
-			return url;
-		};
-
-		if (!argOrginUrl || "" == argOrginUrl) {
-			throw new Error("orgin url null.");
-		}
-		var index = argOrginUrl.indexOf("?");
-		if (-1 != index) {
-			baseurl = argOrginUrl.substring(0, index);
-			if (index + 1 < argOrginUrl.length - 1) {
-				var querystring = argOrginUrl.substring(index + 1);
-				var kvss = querystring.split("&");
-				for (var i = 0; i < kvss.length; i++) {
-					var kvs = kvss[i].split("=");
-					if (1 >= kvs.length) {
-						continue;
-					}
-					_this.putParam(kvs[0], kvs[1]);
-				}
-			}
-		} else {
-			baseurl = argOrginUrl;
-		}
-	};
-
 	kzg.on(window, "load", function () {
 		kzg.fire(kzg, 'ok');
 	});
